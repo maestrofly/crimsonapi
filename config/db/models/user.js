483b5ae219cc921f786eec83f9f424c1c2bab776
maestrofly <derrickalberto@ymail.com>
2024-01-23 16:23:10 +0000

    middleware and routes

--- config/db/models/user.js
+++ config/db/models/user.js
@@ -21,7 +21,8 @@ const userSchema = new mongoose.Schema({
                     throw new Error( 'Email is invalid' )
                      }
                 }
-      }
+      },
+
     password: {
         type: String,
         required: true,
@@ -42,6 +43,40 @@ const userSchema = new mongoose.Schema({
     }, {
     timestamps: true
     })
+
+    //Generate auth token
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString()}, process.env.JWT_SECRET)
+  user.tokens = user.tokens.concat({token})
+   await user.save()
+  return token
+}
+
+//login in users
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+  if (!user) {
+      throw new Error('Unable to log in')
+  }
+  const isMatch = await bcrypt.compare(password, user.password)
+  console.log(isMatch)
+  if(!isMatch) {
+      throw new Error('Unable to login')
+  }
+
+  return user
+}
+
+//Hash plain password before saving
+userSchema.pre('save', async function(next) {
+  const user = this
+  if (user.isModified('password')) {
+      user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
+})
 
     const User = mongoose.model('User', userSchema)
 module.exports = User
